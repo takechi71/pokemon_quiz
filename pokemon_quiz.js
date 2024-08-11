6b3da1df6b5d20b081e47acd8634c38be9922f9f
takechi71 <s248727q@st.go.tuat.ac.jp>
2024-08-11 20:47:52 +0900

    commit

--- pokemon_quiz.js
+++ pokemon_quiz.js
@@ -11,24 +11,14 @@ function getUniqueRandomNumbers(min, max, count) {
 }
 
 const apiUrl1 = "https://pokeapi.co/api/v2/pokemon/";   // ポケモンの基本情報を取得するためのURL
-const apiUrl2 = "https://pokeapi.co/api/v2/pokemon-species/";   // ポケモンの基本情報以外の情報を取得するためのURL
+const apiUrl2 = "https://pokeapi.co/api/v2/pokemon-species/";   // 上記のURLでは取得できない情報を取得するためのURL
 
 document.getElementById("get").addEventListener("click", async () => {
   const array_id = getUniqueRandomNumbers(1, 493, 4);
-  const response1 = await fetch(apiUrl1 + array_id[random]);
-  const response2 = await fetch(apiUrl2 + array_id[random]);
-  
-  const response3 = await fetch(apiUrl2 + array_id[1]);
-  const response4 = await fetch(apiUrl2 + array_id[2]);
-  const response5 = await fetch(apiUrl2 + array_id[3]);
+  ansIndex = Math.floor(Math.random() * 4);   // 答えとするポケモンをランダムに決める.
+  const responses = await Promise.all(array_id.map(id => fetch(apiUrl1 + id)));
+  const pokemons = await Promise.all(responses.map(response => response.json()));
 
-  const pokemon1_p1 = await response1.json();
-  const pokemon1_p2 = await response2.json();
-  
-  const pokemon2_p1 = await response3.json();
-  const pokemon3_p1 = await response4.json();
-  const pokemon4_p1 = await response5.json();
-  
   const image = document.createElement("img");
   const name1 = document.createElement("p");
   const name2 = document.createElement("p");
@@ -40,15 +30,15 @@ document.getElementById("get").addEventListener("click", async () => {
   const color = document.createElement("p");
   const shape = document.createElement("p");
 
-  image.src = pokemon1_p1.sprites.front_default;
-  name1.textContent = `Name: ${pokemon1_p1.name}`;
-  name2.textContent = `Name: ${pokemon2_p1.name}`;
+  image.src = pokemon1.sprites.front_default;
+  name1.textContent = `Name: ${pokemon1.name}`;
+  name2.textContent = `Name: ${pokemon2.name}`;
   name3.textContent = `Name: ${pokemon3_p1.name}`;
   name4.textContent = `Name: ${pokemon4_p1.name}`;
-  weight.textContent = `Weight: ${pokemon1_p1.weight / 10} kg`;
-  height.textContent = `Height: ${pokemon1_p1.height / 10} m`;
-  type.textContent = `Type: ${pokemon1_p1.types.map(t => t.type.name).join(", ")}`;
-  color.textContent = `Color: ${pokemon1_p2.color.name}`;
+  weight.textContent = `Weight: ${pokemon1.weight / 10} kg`;
+  height.textContent = `Height: ${pokemon2ight / 10} m`;
+  type.textContent = `Type: ${pokemon1.ty2map(t => t.type.name).join(", ")}`;
+  color.textContent = `Color: ${pokem2p2.color.name}`;
   shape.textContent = `shape: ${pokemon1_p2.shape.name}`;
   
   const pokemonDiv = document.getElementById("pokemon");
