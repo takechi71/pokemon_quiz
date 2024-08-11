af1bd8f15f1a933b0a7ebb8a8139362d79ade9fb
takechi71 <s248727q@st.go.tuat.ac.jp>
2024-08-11 11:23:01 +0900

    function getUniqueRandomNumbers

--- pokemon_quiz.js
+++ pokemon_quiz.js
@@ -1,6 +1,24 @@
-const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
+// min～maxの範囲で、count数分の乱数を生成するgetUniqueRandomNumbers関数.
+function getUniqueRandomNumbers(min, max, count) {
+  const uniqueNumbers = new Set();
+
+  while (uniqueNumbers.size < count) {
+      const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+      uniqueNumbers.add(randomNum);
+  }
+
+  return Array.from(uniqueNumbers);
+}
+
+const apiUrl1 = "https://pokeapi.co/api/v2/pokemon/";   // ポケモンの基本情報を取得するためのURL
+const apiUrl2 = "https://pokeapi.co/api/v2/pokemon-species/";   // ポケモンの基本情報以外の情報を取得するためのURL
 
 document.getElementById("get").addEventListener("click", async () => {
+  const array_id = getUniqueRandomNumbers(1, 493, 4);
+  const response1 = await fetch(apiUrl1 + array_id[0]);
+  const response2 = await fetch(apiUrl2 + array_id[0]);
+  parameter1 = await response1.json();
+  parameter2 = await response2.json();
   const response = await fetch(apiUrl + Math.floor(Math.random() * 493));
   const pokemon = await response.json();
   const image = document.createElement("img");
